7eb17d20e33b36d7eb374c8ea71052fa15fe30ac
Mega Govania <megagovania@gmail.com>
2022-04-26 15:04:03 +0700

    Initial Commit

---
+++ NC1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NC1
+//
+//  Created by Mega Govania on 26/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
